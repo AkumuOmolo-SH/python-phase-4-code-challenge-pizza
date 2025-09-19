a4ad8cafc9c81c5d7183e490f8a614b1b0235a4c
AkumuOmolo <akumu.omoloh@gmail.com>
2025-09-19 14:16:03 +0300

    Adds get restaurant by id and delete

--- server/app.py
+++ server/app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify, abort
 from flask_restful import Api, Resource
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
@@ -24,15 +25,27 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
-@app.route("/restaurants", method = ["GET"])
+
+@app.route("/restaurants", methods=["GET"])
 def get_endpoint():
     restaurants = [r.to_dict() for r in Restaurant.query.all()]
     return jsonify(restaurants), 200
 
 
+@app.route("/restaurants/<int:id>", methods=["GET", "DELETE"])
+def restaurant_by_id(id):
+    restaurant = Restaurant.query.filter(Restaurant.id == id).first()
 
-    
+    if not restaurant:
+        return jsonify({"error": "Restaurant not found"}), 404
 
+    if request.method == "GET":
+        return jsonify(restaurant.to_dict()), 200
+
+    elif request.method == "DELETE":
+        db.session.delete(restaurant)
+        db.session.commit()
+        return jsonify({"message": "Restaurant deleted"}), 200
 
 
 if __name__ == "__main__":
